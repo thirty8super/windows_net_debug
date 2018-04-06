11b2510fc6f558349c6d8d7def12841eb2b2a830
David Cox <dcox@bmc.com>
2018-04-06 13:01:15 -0500

    Added more debug endpoints.   Fixed file encoding

--- win_network_monitor.ps1
+++ win_network_monitor.ps1
@@ -1,3 +1,5 @@
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
 $hour = 0
 $min = 0
 
@@ -12,11 +14,13 @@ while(1) {
 		$min = 0
 	}
 
-	Get-Date *> sample-$hour-$min.txt
-	Get-NetAdapter *>> sample-$hour-$min.txt
-	Get-NetIPConfiguration *>> sample-$hour-$min.txt
-	Test-NetConnection www.google.com *>> sample-$hour-$min.txt
-	Test-NetConnection api.truesight.bmc.com *>> sample-$hour-$min.txt
+	Get-Date -Out-File  sample-$hour-$min.txt
+	Get-NetAdapter -Out-File -Append sample-$hour-$min.txt
+	Get-NetIPConfiguration -Out-File -Append sample-$hour-$min.txt
+	Test-NetConnection www.google.com -Out-File -Append sample-$hour-$min.txt
+	Test-NetConnection api.truesight.bmc.com -Out-File -Append sample-$hour-$min.txt
+	iwr -Method HEAD https://api.truesight.bmc.com -Out-File -Append sample-$hour-$min.txt
+	iwr -Method HEAD https://help.truesight.bmc.com -Out-File -Append sample-$hour-$min.txt
 
 	$min = $min + 15
 
